24ffbe805595673221e8a3c8c6d51605dcabe6cf
dave <spamspammaps@gmail.com>
2015-11-16 11:39:42 -0600

    git made me commit.. grrrrr some crappy code has been made

---
+++ Trainingset.py
@@ -0,0 +1,18 @@
+from random import *
+class Trainingset
+    __init__()
+    
+    
+    def mash(input1_path,input2_path,output_path)
+        inputfile1=file.open(input1_path)
+        inputfile2=file.open(input2_path)
+        
+        inputstring1= inputfile1.Read()
+        inputstring2= inputfile2.Read()
+        
+        
+    def annotate()
+        
+        
+Trainingset set1= Trainingset()
+set1.mash("./links.txt","./random.txt","output.txt")
