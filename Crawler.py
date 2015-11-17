81adbb5611c764bf744bca0837b7779dd25b2cc8
Matt Heller <mjheller89@gmail.com>
2015-11-17 08:35:47 -0600

    added html/css files to display our data

--- Crawler.py
+++ Crawler.py
@@ -1,29 +1,37 @@
-import scrapy
-from scrapy.contrib.spiders import CrawlSpider, Rule
-from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
-from scrapy.selector import HtmlXPathSelector, Selector
+from scrapy.spiders.Spider import BaseSpider, CrawlSpider, Rule
+from scrapy.selector import HtmlXPathSelector
+from scrapy.linkextractors import LinkExtractor
+from scrapy.http import Request
+import time
 
-class wikiSpider(CrawlSpider)
-	name = 'wikiSpider'
-	allowed_domains = ["wikipedia.org"]
+class wikiSpider(BaseSpider):
+    name = 'wikiSpider'
+    allowed_domains = ["wikipedia.org"]
     start_urls = ["http://en.wikipedia.org/wiki/Main_Page"]
-	
+    
     rules = (
-    	Rule(SgmlLinkExtractor(restrict_xpaths=('//div[@class="mw-body-content"//a/@href'))),
-    	Rule(SgmlLinkExtractor( allow=("http://en.wikipedia.org/wiki",)),callback="parse_item"),
-		)
-	
-	def parse_item(self, response):
+        Rule(LinkExtractor(restrict_xpaths=('//div[@class="mw-body-content"//a/@href'))),
+        Rule(LinkExtractor( allow=("http://en.wikipedia.org/wiki",)),callback="parse_item"),
+        )
+    
+    def parse_item(self, response):
         hxs = HtmlXPathSelector(response)
-        #print hxs.select('//h1[@class = "firstHeading"]/span/text()').extract()
-        #heading = hxs.select('//h1[@class = "firstHeading"]/span/text()').extract()
-		sites = hxs.select('//table[@id="mp-upper"]/tr')
-		#sites = hxs.select('//div[@class="mw-body-content"//a/@href')
+        #titles = hxs.select('//h1[@class = "firstHeading"]/span/text()').extract()
+        #print titles
+        content = hxs.select('//table[@id="mp-upper"]/tr')
+        #sites = hxs.select('//div[@class="mw-body-content"//a/@href')
+        crawledLinks = []
         items = []
-        for site in sites:
+
+        for item in content:
             item = WikipediaItem()
             item['title'] = site.select('.//a/text()').extract()
-            item['link'] = site.select('.//a/@href').extract()
-            item['details'] = site.select('.//p/text()').extract()
+            #item['link'] = site.select('.//a/@href').extract()
+            #item['details'] = site.select('.//p/text()').extract()
             items.append(item)
-        return items
+            print item
+        #time.sleep(1)
+        #return items
+
+test = wikiSpider()
+test.parse_item()
