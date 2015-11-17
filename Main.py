b8931527e1109e067a8d5615ef271849eb63bb8a
Alex Morask <Alex Morask>
2015-11-17 11:15:11 -0600

    Modifying index.html to include variable testing

--- Main.py
+++ Main.py
@@ -4,12 +4,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template('home.html')
-
-@app.route('/about')
-def about():
-	return render_template('about.html')
-
-
-if __name__ == '__main__':
-	app.run(debug=True)
+	return render_template('index.html', name = "Alex")
