18ab37bb302edf95afcfdd3243256c5ffd8cb838
Alex Morask <Alex Morask>
2015-11-17 17:51:48 -0600

    Removing Comments from Main

--- Main.py
+++ Main.py
@@ -1,23 +1,19 @@
 from flask import Flask, render_template, redirect, request
 import os
 
-
 app = Flask(__name__)
 handle = connect()
 
 @app.route('/index', methods=['GET'])
 @app.route('/', methods =['GET'])
 def index():
-	#bind the index page to the database
-	#userinputs = [x for x in handle.mycollection.find()]
-	return render_template('index.html', name= "Ryan")
+	return render_template('index.html')
 
 @app.route('/write', methods=['POST'])
 def write():
-	userinput= request.form.get("userinput")
-	#oid = handle.mycollection.insert({"message": userinput})
-	return redirect ("/")
+	user_input = request.form("user-input")
+	return redirect 
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT, 5000'))
-    app.run(host="10.2.20.39", port=port, debug = True)
+    app.run(host = "10.2.20.12", port = port, debug = True)
