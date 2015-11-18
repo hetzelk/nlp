8dcc4cce2f78aee35363365f27b355b1bb8fa03d
mcnuggz <dzanfox@gmail.com>
2015-11-18 10:40:07 -0600

    Outlayed a basic layout for the flask webpage

--- Main.py
+++ Main.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template, redirect, request
 import os
 
-
 app = Flask(__name__)
-handle = connect()
 
 @app.route('/index', methods=['GET'])
 @app.route('/', methods =['GET'])
@@ -17,7 +15,7 @@ def write():
 	userinput= request.form.get("userinput")
 	#oid = handle.mycollection.insert({"message": userinput})
 	return redirect ("/")
-
+# host="10.2.20.39", port=port, 
 if __name__ == '__main__':
-	port = int(os.environ.get('PORT, 5000'))
-    app.run(host="10.2.20.39", port=port, debug = True)
+    # port = int(os.environ.get('PORT, 5000'))
+    app.run(debug = True)
