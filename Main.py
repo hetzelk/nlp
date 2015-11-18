b80e2f4e253c8b6effca1dcd70beb798e18c0cff
root <roberttmoon@gmail.com>
2015-11-18 11:06:04 -0500

    commited out lines in Main.py for testing

--- Main.py
+++ Main.py
@@ -4,16 +4,17 @@ import os
 app = Flask(__name__)
 handle = connect()
 
-@app.route('/index', methods=['GET'])
+#@app.route('/index', methods=['GET'])
 @app.route('/', methods =['GET'])
 def index():
-	return render_template('index.html')
+	#return render_template('index.html')
+	return "hello, world"
 
-@app.route('/write', methods=['POST'])
-def write():
-	user_input = request.form("user-input")
-	return redirect 
+#@app.route('/write', methods=['POST'])
+#def write():
+#	user_input = request.form("user-input")
+#	return redirect 
 
 if __name__ == '__main__':
-	port = int(os.environ.get('PORT, 5000'))
-    app.run(host = "10.2.20.12", port = port, debug = True)
+#    port = int(os.environ.get('PORT, 5000'))
+    app.run(host = "10.2.20.12")
