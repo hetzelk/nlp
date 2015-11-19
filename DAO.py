ef3526f9afff3077a677f353f98443a4d7252bd3
Alex Morask <Alex Morask>
2015-11-19 14:22:17 -0600

    Adding Collection argument to find

--- DAO.py
+++ DAO.py
@@ -8,8 +8,8 @@ class DAO:
         self.handle = self.connection[keys['DATABASE']]
         self.handle.authenticate(keys['USER_NAME'], keys['PASSWORD'])
     
-    def insert_one(self, entry):
-        id = self.handle[keys['COLLECTIONS'][2]].insert_one(entry).inserted_id
+    def insert_one(self, entry, collection):
+        id = self.handle[collection].insert_one(entry).inserted_id
         return id
     
     def find(self, collection, key):
