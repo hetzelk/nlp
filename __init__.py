4b92b9336aaf13d44d449a85e1cad449812bb3e6
Alex Morask <Alex Morask>
2015-11-20 08:44:23 -0600

    Fixed Server Error for /search

--- __init__.py
+++ __init__.py
@@ -2,16 +2,20 @@ from flask import Flask, render_template, redirect, request, url_for
 import os
 from DAO import DAO
 from Wikipedia_API import Wikipedia_API
+import sys, traceback
+from run_comparison import Compare
 
 app = Flask(__name__)
 ## dao = DAO()
-## api = Wikipedia_API()
 
-@app.route('/index', methods= ['GET'])
+@app.route('/index', methods = ['GET'])
 @app.route('/', methods = ['GET'])
 def index():
-    return render_template('index.html')
-
+    try:
+        return render_template('index.html')
+    except Exception as e:
+        traceback(e)
+        
 @app.route('/training-set', methods = ['GET', 'POST'])
 def training_set():
     try:
@@ -19,18 +23,29 @@ def training_set():
         ## titles = [title.title() for title in titles]
         return render_template('training-set.html')
     except Exception as e:
-        print(e)
+        traceback(e)
     
-@app.route('/search', methods=['POST'])
+@app.route('/search', methods = ['POST'])
 def search():
-    search = request.form.get("search")
-    compare = Compare()
-    context = {'searched': True, 'query_name': compare.query_name, 'results': compare.top}
-    return redirect(url_for('index', **context))
+    try:
+        search = request.form.get("search")
+        comparison = Compare()
+        context = {'searched': True, 'query_name': compare.query_name, 'results': compare.top}
+        return redirect(url_for('index'))
+    except Exception as e:
+        traceback(e)
 
 @app.route('/quote')
 def quote():
-    return render_template('quote.html')
+    try:
+        return render_template('quote.html')
+    except Exception as e:
+        traceback(e)
+    
+def traceback(e):
+    ex_type, ex, tb = sys.exc_info()
+    traceback.print_tb(tb)
+    print("Error: {} | Type: {}".format(e, type(e)))
 
 if __name__ == '__main__':
     app.run()
